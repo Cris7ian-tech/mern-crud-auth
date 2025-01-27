29fcf230b69e64b45bfc81b8245f8af7ad9ab006
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-01-27 04:20:58 -0300

    register, login, logout

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -4,6 +4,8 @@ import bcrypt from 'bcryptjs';
 import {createAccesToken} from '../libs/jwt.js';
 
 
+
+
 export const register = async (req, res) => {
   const {username, email, password} = req.body
 
@@ -37,4 +39,41 @@ export const register = async (req, res) => {
   }
 };
 
-export const login = (req, res) => res.send('Login');
+export const login = async (req, res) => {
+  const {email, password} = req.body
+
+  //create user
+  try {
+    const userFound = await User.findOne({email})
+
+    if(!userFound) return res.status(400).json({message: "Usuario no encontrado"});
+    
+    //si encuentra el usuario: compara
+    const isMatch = await bcrypt.compare(password, userFound.password); 
+
+    if(!isMatch) return res.status(400).json({message: "Contraseña incorrecta"});
+
+
+    const token =await createAccesToken({id: userFound._id});
+    
+    res.cookie("token", token);
+    //devolver el dato al frontend:
+    res.json({
+      id: userFound._id,
+      username: userFound.username,
+      email: userFound.email,
+      createdAt: userFound.createdAt,
+      updatedAt: userFound.updatedAt,
+    });
+  } catch (error) {
+    res.status(500).json({message: "Usuario no encontrado"});
+    console.log(error);
+  }
+};
+
+export const logout = (req, res) => {
+  res.cookie = ('token', "", {
+    expires: new Date(0)
+  });
+  return res.sendStatus(200);
+}
