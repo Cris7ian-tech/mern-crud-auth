5c3ba63c17a67b8383710e175a5c465ca8523c3e
Cristian Lucero/ <cristian_ala2@hotmail.com>
2025-01-27 03:37:06 -0300

    jwt creado mas cookie

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,21 +1,38 @@
 import User from '../models/user.model.js';
 import bcrypt from 'bcryptjs';
 
+import {createAccesToken} from '../libs/jwt.js';
+
+
 export const register = async (req, res) => {
   const {username, email, password} = req.body
 
   //create user
   try {
+
+    //hash password
+    const passwordHash = await bcrypt.hash(password, 10); 
+
     const newUser = new User({
       username, 
       email, 
-      password
+      password: passwordHash,
     });
 
     const userSaved = await newUser.save();
+    const token =await createAccesToken({id: userSaved._id});
+    
+    res.cookie("token", token);
     //devolver el dato al frontend:
-    res.json(userSaved);
+    res.json({
+      id: userSaved._id,
+      username: userSaved.username,
+      email: userSaved.email,
+      createdAt: userSaved.createdAt,
+      updatedAt: userSaved.updatedAt,
+    });
   } catch (error) {
+    res.status(500).json({message: "Error al registrar usuario"});
     console.log(error);
   }
 };
